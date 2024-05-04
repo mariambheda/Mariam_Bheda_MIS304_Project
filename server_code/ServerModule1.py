54012152e626a87090d97e81f2021232d63ab795
mariambheda@gmail.com <mariambheda@gmail.com>
2024-05-04 08:50:37 +0000

    Created IMG_8035.JPG, IMG_8036.JPG, IMG_8037.JPG, IMG_8039.JPG and 17 more; Edited ServerModule1 and Form1; Deleted Form1.ItemTemplate1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -7,20 +7,6 @@ import anvil.media
 
 
 
-# Define the server function to process the uploaded image
-
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
 @anvil.server.callable
 def send_pin_locs():
   rows = tables.app_tables.locations.search()
